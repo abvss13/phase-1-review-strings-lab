b12b6ae9e08b4d1cfffe0ef2545b94bd6468afdf
Your Name <abdullahi.abass@student.moringschool.com>
2023-06-14 15:46:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = "Abass";
-const welcomeMessage = "Welcome to Flatbook" + currentUser + "!";
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.charAt(0) + "!";
-const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!"; 
